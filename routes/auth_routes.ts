eeaeb764b8ba12b1b43f364aa157fe20572ef8d2
perhac <abdullah13112004@gmail.com>
2024-08-12 20:41:04 +0530

    feat: auth routes

---
+++ routes/auth_routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { login, register } from "../controllers/auth_controller";
+
+const router = express.Router();
+
+router.post("/login", login);
+router.post("/signup", register);
+
+export default router;
