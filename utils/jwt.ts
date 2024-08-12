776227c5ea2f530a5aaec0d8ad73eb138c12d217
perhac <abdullah13112004@gmail.com>
2024-08-12 20:39:22 +0530

    feat!: utils for jwt, hashed password

---
+++ utils/jwt.ts
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import { Response } from "express";
+
+const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
+
+interface TokenPayload {
+  email: string;
+  accessApproved: boolean;
+  role: string;
+}
+
+interface JwtPayload {
+  payload: TokenPayload;
+  iat: number;
+  exp: number;
+}
+
+export const generateTokenAndSetCookie = (
+  payload: TokenPayload,
+  res: Response
+): string => {
+  try {
+    const token = jwt.sign({ payload }, JWT_SECRET, {
+      expiresIn: "15d",
+    });
+
+    res.cookie("ver-user", token, {
+      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+      httpOnly: true, // prevent XSS attacks
+      secure: process.env.NODE_ENV !== "development", // use secure cookies in production
+    });
+
+    return token;
+  } catch (error) {
+    console.error("Error generating token:", error);
+    res.status(500).send("Internal Server Error");
+    return "Token error";
+  }
+};
+
+export const verifyToken = (token: string): JwtPayload => {
+  return jwt.verify(token, JWT_SECRET) as JwtPayload;
+};
