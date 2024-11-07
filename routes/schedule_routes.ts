ad6ae8cde0c55be2583afcd0c14f89b0550817ba
PerHac13 <abdullah13112004@gmail.com>
2024-11-07 11:49:48 +0530

    fix!: minor changes

--- routes/schedule_routes.ts
+++ routes/schedule_routes.ts
@@ -1,19 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   createSchedule,
   updateSchedule,
   getAllSchedules,
+  getScheduleByUser,
   getScheduleById,
-} from '../controllers/schedule_controller';
-import { authenticate } from '../middleware/auth_middleware';
-import { authorized } from '../middleware/checkPermission';
-import { Role } from '../constants/role';
+} from "../controllers/schedule_controller";
+import { authenticate } from "../middleware/auth_middleware";
+import { authorized } from "../middleware/checkPermission";
+import { Role } from "../constants/role";
 
 const router = Router();
 
-// Get all schedules
 router.get(
-  '/',
+  "/",
   authenticate,
   authorized([Role.ADMIN, Role.USER]),
   getAllSchedules
@@ -21,25 +21,25 @@ router.get(
 
 // Create a new schedule
 router.post(
-  '/',
+  "/",
   authenticate,
-  authorized([Role.ADMIN,Role.USER]),
+  authorized([Role.ADMIN, Role.USER]),
   createSchedule
 );
 
 // Get a specific schedule by ID
 router.get(
-  '/:id',
+  "/:id",
   authenticate,
-  authorized([Role.ADMIN,Role.USER]),
+  authorized([Role.ADMIN, Role.USER]),
   getScheduleById
 );
 
 // Update an existing schedule
 router.put(
-  '/:id',
+  "/:id",
   authenticate,
-  authorized([Role.ADMIN,Role.USER]),
+  authorized([Role.ADMIN, Role.USER]),
   updateSchedule
 );
 
